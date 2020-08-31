d3636555546a218f0caf2746da14d68b2518ba49
Mario Salinas <mario_1605@live.com.mx>
2020-08-31 09:28:28 -0500

    feat: logo component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories/index'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards/index'
 import { GlobalStyle } from './GlobalStyles'
+import { Logo } from './components/Logo/index'
 
 export const App = () => (
   <>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </>
