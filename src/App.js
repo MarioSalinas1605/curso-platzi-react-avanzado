bcc6eb745944e48b0d225eafd856f4daf0288dbe
Mario Salinas <mario_1605@live.com.mx>
2020-08-31 07:22:40 -0500

    feat: PhotoCard and ListOfPhotoCards components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories/index'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards/index'
 import { GlobalStyle } from './GlobalStyles'
 
 export const App = () => (
   <>
     <GlobalStyle />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </>
 )
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
+
 import { Category } from '../Category/index'
 import { List, Item } from './style'
+import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
   return (
     <List>
       {
-        [1, 2, 3, 4, 5, 6, 7, 8].map(category => <Item key={category}><Category /></Item>)
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
   )
