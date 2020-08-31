c0b8e4faf18e7a74e2584b9c5c18b490ae311fc1
Mario Salinas <mario_1605@live.com.mx>
2020-08-31 10:29:22 -0500

    feat: animation for image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories/index'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards/index'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo/index'
 
 export const App = () => (
