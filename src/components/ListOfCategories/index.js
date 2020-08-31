abc80028b88435bdcec51e3c5e2f670b7f30d8d2
Mario Salinas <mario_1605@live.com.mx>
2020-08-31 12:47:45 -0500

    feat: categories fetch and fixed bar

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Category } from '../Category/index'
 import { List, Item } from './style'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(() => {
+    window.fetch('https://petgram-server-mario.mariosalinas1605.vercel.app/categories')
+      .then(res => res.json())
+      .then(data => {
+        setCategories(data)
+      })
+  }, [])
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
