d56fcc2af2d8d8f69c98a8fc59eeeeac1c2d11dc
Mario Salinas <mario_1605@live.com.mx>
2020-09-12 13:02:20 -0500

    feat: navegation bar

---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+import { Link as LinkRouter } from '@reach/router'
+
+export const Nav = styled.nav`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    height: 50px;
+    margin: 0 auto;
+    max-width: 500px;
+    width: 100%;
+    background: #fcfcfc;
+    border-top: 1px solid #e0e0e0;
+    z-index: 100;
+`
+export const Link = styled(LinkRouter)`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 100%;
+    text-decoration: none;
+    color: #888;
+    width: 100%;
+`
