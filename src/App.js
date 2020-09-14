7c6d37bf82ea4d55ca723d488e6ff53f8c404334
Mario Salinas <mario_1605@live.com.mx>
2020-09-14 07:00:48 -0500

    feat: protecting private routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo/index'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { NavBar } from './components/NavBar/index'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 
 export const App = () => {
   return (
@@ -17,6 +24,20 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              : <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+        }
+      </UserLogged>
       <NavBar />
     </>
   )
