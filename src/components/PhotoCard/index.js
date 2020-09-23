9941267f62a04bc563f811ee02262acf7d9ca0c3
Mario Salinas <mario_1605@live.com.mx>
2020-09-22 20:22:10 -0500

    feat: user like and control status of user

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,22 +1,18 @@
 import React from 'react'
 import { Link } from '@reach/router'
 
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton/index'
 import { useToggleLike } from '../../hooks/useToggleLike'
 import { ImgWrapper, Img, Article } from './styles'
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const { toggleLikePhoto } = useToggleLike(id)
 
   const handleFavClick = () => {
-    !liked && toggleLikePhoto()
-    setLiked(!liked)
+    toggleLikePhoto()
   }
 
   return (
--- src/index.js
+++ src/index.js
@@ -7,7 +7,26 @@ import { App } from './App'
 import Context from './Context'
 
 const client = new ApolloClient({
-  uri: 'https://petgram-server-mario.mariosalinas1605.vercel.app/graphql'
+  uri: 'https://petgram-server-mario.mariosalinas1605.vercel.app/graphql',
+  request: operation => {
+    const token = window.sessionStorage.getItem('token')
+    const authorization = token ? `Bearer ${token}` : ''
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    })
+  },
+  onError: error => {
+    console.log('-*- Error')
+    console.log(error)
+    const { response } = error
+    console.log(response)
+    if (response && response.errors[0].message === 'you must be logged in to perform this action') {
+      window.sessionStorage.removeItem('token')
+      window.location.href = '/favs'
+    }
+  }
 })
 
 ReactDOM.render(
