83a31869d67cb054f91926abe8a1e5ebfcc4ffb9
Mario Salinas <mario_1605@live.com.mx>
2020-09-23 15:51:15 -0500

    refactor: routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react'
-import { Router } from '@reach/router'
+import React, { useContext } from 'react'
+import { Redirect, Router } from '@reach/router'
 
-import Context from './Context'
+import { Context } from './Context'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo/index'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
+import { NotFound } from './pages/NotFound'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { NavBar } from './components/NavBar/index'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
+        {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+        {isAuth && <Redirect noThrow from='/login' to='/' />}
       </Router>
       <Context.Consumer>
         {
